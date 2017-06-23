324c2bc7ed91fca074400ea8e0be4a856a5571f0
Allen Zhong <zhong.allen@gmail.com>
2017-06-23 18:20:06 +1200

    add customized character

--- src/components/CarouselIndicator.js
+++ src/components/CarouselIndicator.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import "./Carousel.css";
 
 export default class CarouselIndicator extends Component {
+
+  constructor(props) {
+    super(props);
+    this.buttonCharacter = this.buttonCharacter.bind(this);
+  }
+
+  buttonCharacter() {
+    return this.props.buttonCharacter ? this.props.buttonCharacter : '\u25a0';
+  }
+
   render() {
     return (
       <div className="carousel--indicators">
@@ -12,7 +22,7 @@ export default class CarouselIndicator extends Component {
                       key={i}
                       data-index={i}
                       onClick={this.props.handleClick}>
-                &#9724;
+                {this.buttonCharacter()}
               </button>
             )
           }
